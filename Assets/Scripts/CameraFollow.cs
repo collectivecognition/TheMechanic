e19581d9422fa3bd39665450455e62df5d2984ad
William Blanchette <William Blanchette>
2016-10-09 15:08:24 -0600

    Tweaking camera

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,17 +4,21 @@ using System.Collections;
 public class CameraFollow : MonoBehaviour {
     public Transform player;
 
-    private float followDistance = 30f;
-    private float elevation = 25f;
-    private float followSpeed = 10;
+    private float followDistance = 25f;
+    private float elevation = 15f;
+    private float followSpeed = 0.3f;
+    private Vector3 velocity = Vector3.zero;
 
     void Update () {
         transform.LookAt(player.transform);
 
-        if (Vector3.Distance(transform.position, player.position) > followDistance) {
+        //if (Vector3.Distance(transform.position, player.position) > followDistance) {
             Vector3 targetPosition = player.position;
-            targetPosition.y = player.position.y + elevation;
-            transform.position = Vector3.MoveTowards(transform.position, targetPosition, followSpeed * Time.deltaTime);
-        }
+            Transform turretTransform = player.transform.Find("Tank/Turret").transform;
+            targetPosition = player.transform.position + (turretTransform.right * followDistance) + (Vector3.up * elevation);
+            Debug.Log(turretTransform.rotation);
+            // targetPosition.y = player.position.y + elevation;
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, followSpeed);
+        //}
 	}
 }
