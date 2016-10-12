bb463f499bcfce8399bf9ab1fbfdb35be998aa72
William Blanchette <William Blanchette>
2016-10-11 20:46:26 -0600

    Switched to fixed 45 degree camera

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -10,11 +10,13 @@ public class CameraFollow : MonoBehaviour {
     private Vector3 velocity = Vector3.zero;
 
     void Update () {
-        transform.LookAt(player.transform);
+        //transform.LookAt(player.transform);
 
-        Vector3 targetPosition = player.position;
-        Transform turretTransform = player.transform.Find("Turret").transform;
-        targetPosition = player.transform.position + (-turretTransform.forward * followDistance) + (Vector3.up * elevation);
-        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, followSpeed);
+        //Vector3 targetPosition = player.position;
+        //Transform turretTransform = player.transform.Find("Turret").transform;
+        //targetPosition = player.transform.position + (-turretTransform.forward * followDistance) + (Vector3.up * elevation);
+        //transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, followSpeed);
+
+        transform.position = player.transform.position + Vector3.up * 30 - Vector3.right * 20 - Vector3.forward * 20;
 	}
 }
