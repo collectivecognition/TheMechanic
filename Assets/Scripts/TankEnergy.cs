db528b634501a097c474c5bd91e24afbd0fddec3
William Blanchette <William Blanchette>
2016-10-14 07:43:23 -0600

    Firing gun now consumes energy

--- Assets/Scripts/TankEnergy.cs
+++ Assets/Scripts/TankEnergy.cs
@@ -3,10 +3,12 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class TankEnergy : MonoBehaviour {
+    public float Energy { get { return energy;}  }
+
     private float energy = 100f;
     private float totalEnergy = 100f;
+    private float rechargeRate = 10f;
     private Image energyBar;
-   
 
     void Start() {
         energyBar = transform.Find("EnergyCanvas/Energy").GetComponent<Image>();
@@ -14,6 +16,8 @@ public class TankEnergy : MonoBehaviour {
 
     void Update() {
         energyBar.fillAmount = energy / totalEnergy;
+
+        energy += rechargeRate * Time.deltaTime;
     }
 
     public void UseEnergy(float amount) {
--- Assets/Scripts/TankGun.cs
+++ Assets/Scripts/TankGun.cs
@@ -35,21 +35,18 @@ public class TankGun : MonoBehaviour {
     }
 
     public void Fire () {
-        //RaycastHit hit;
-        Vector3 pos = firingPoint.position;
+        TankEnergy energy = GetComponent<TankEnergy>();
 
-        // Debug.DrawRay(pos, firingPoint.forward * 1000f, Color.red, 101f, true);
+        if (energy.Energy >= 40f) {
+            Vector3 pos = firingPoint.position;
 
-        //if (Physics.Raycast(pos, firingPoint.forward, out hit, Mathf.Infinity)) {
-        //    if(hit.collider.tag == "Enemy" || hit.collider.name == "Player") {
-        //        hit.collider.transform.GetComponent<TankGun>().Hit(50f);
-        //    }
-        //}
+            GameObject projectile = GameObject.Instantiate(projectilePrefab);
+            projectile.transform.position = pos;
+            projectile.transform.rotation = turret.rotation;
 
-        GameObject projectile = GameObject.Instantiate(projectilePrefab);
-        projectile.transform.position = pos;
-        projectile.transform.rotation = turret.rotation;
+            projectile.GetComponent<Rigidbody>().AddForce(projectile.transform.forward * 30f, ForceMode.Impulse);
 
-        projectile.GetComponent<Rigidbody>().AddForce(projectile.transform.forward * 30f, ForceMode.Impulse);
+            energy.UseEnergy(40f);
+        }
     }
 }
