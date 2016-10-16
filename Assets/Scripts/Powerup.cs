ba318e6a389ffb54eef78ab6670433a1c86cc620
William Blanchette <William Blanchette>
2016-10-15 22:27:36 -0600

    Added picking up powerups

--- Assets/Scripts/Powerup.cs
+++ Assets/Scripts/Powerup.cs
@@ -5,4 +5,12 @@ public class Powerup : MonoBehaviour {
 	void Start (){
         iTween.RotateAdd(gameObject, iTween.Hash("y", 180f, "time", 1.5f, "loopType", iTween.LoopType.loop, "easeType", iTween.EaseType.linear));
 	}
+
+    void OnCollisionEnter(Collision collision) {
+        if(collision.collider.name == "Player") {
+            GetComponent<ParticleSystem>().Play();
+            GetComponent<Renderer>().enabled = false;
+            GameObject.Destroy(gameObject, 0.5f);
+        }
+    }
 }
