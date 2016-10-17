5b371a92655dda89906b65bfdaba26b5bf8d7b3b
William Blanchette <William Blanchette>
2016-10-16 21:02:48 -0600

    WIP inventory

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -1,15 +1,33 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Inventory : MonoBehaviour {
+    private GameObject inventoryItemPrefab;
+    private List<GameObject> items;
+    private int currentItem = -1;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void Awake() {
+        inventoryItemPrefab = Resources.Load<GameObject>("Prefabs/InventoryItem");
+
+        items = new List<GameObject>();
+        for(int ii = 0; ii < 10; ii++) {
+            GameObject item = Instantiate(inventoryItemPrefab);
+            item.transform.SetParent(transform.Find("Items/Content"), false);
+            item.transform.Find("Text").GetComponent<Text>().text = "This is a test: " + ii;
+            items.Add(item);
+        }
+
+        ChangeItem(8);
+    }
+
+    void ChangeItem(int index) {
+        if (currentItem != -1) {
+            items[currentItem].GetComponent<Image>().color = Color.black;
+        }
+        currentItem = index;
+        items[currentItem].GetComponent<Image>().color = Color.red;
+        items[currentItem].GetComponent<Button>().Select();
+    }
 }
