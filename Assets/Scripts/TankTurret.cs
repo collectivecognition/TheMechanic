b51ac04524c04b1049992acd31654b7f716c5f5e
William Blanchette <William Blanchette>
2016-11-02 10:49:40 -0600

    Fixed some small aiming bugs

--- Assets/Scripts/TankTurret.cs
+++ Assets/Scripts/TankTurret.cs
@@ -14,6 +14,7 @@ public class TankTurret : MonoBehaviour {
 
     private void FixedUpdate() {
         if (!GameManager.Instance.gameActive) return;
+
         if (!BattleManager.Instance.BattleActive) {
             turretTransform.localRotation = bodyTransform.localRotation;
             Cursor.visible = false;
@@ -23,12 +24,14 @@ public class TankTurret : MonoBehaviour {
         float h = Input.GetAxis("TurretHorizontal");
         float v = Input.GetAxis("TurretVertical");
 
+        // Keyboard and analog stick aiming
+
         if (h != 0 || v != 0) {
             Cursor.visible = false;
             Vector3 targetPoint = Vector3.zero;
 
-            targetPoint += Vector3.right * h;
-            targetPoint += Vector3.forward * v;
+            targetPoint += Vector3.right * v;
+            targetPoint += Vector3.forward * h;
 
             if (targetPoint == Vector3.zero) {
                 turretTransform.localRotation = Quaternion.Slerp(turretTransform.localRotation, bodyTransform.rotation * Quaternion.Euler(0, -270, 0), turnSpeed * Time.deltaTime);
@@ -38,9 +41,13 @@ public class TankTurret : MonoBehaviour {
                 targetRotation *= Quaternion.Euler(0, 135, 0);
                 turretTransform.localRotation = Quaternion.Slerp(turretTransform.localRotation, targetRotation, turnSpeed * Time.deltaTime);
             }
+
+        // Mouse aiming
+
         } else if(oldMousePos != Input.mousePosition) { // Only rotate if player is actually using mouse
             oldMousePos = Input.mousePosition;
             Cursor.visible = true;
+
             Plane plane = new Plane(Vector3.up, new Vector3(0f, 15f, 0f)); // 15f is the turret height
             float hit;
             Ray ray = GameManager.Instance.renderCamera.ScreenPointToRay(Input.mousePosition);
@@ -51,7 +58,7 @@ public class TankTurret : MonoBehaviour {
                 current.y = 15f;
 
                 Quaternion targetRotation = Quaternion.LookRotation(target - current);
-                targetRotation *= Quaternion.Euler(0, 90, 0);
+                targetRotation *= Quaternion.Euler(0, 45, 0);
                 turretTransform.localRotation = Quaternion.Slerp(turretTransform.localRotation, targetRotation, turnSpeed * Time.deltaTime);
             }
         }
