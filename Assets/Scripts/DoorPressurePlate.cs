3cb08c2a09801e236cf9e11c8fd6a101a957a166
William Blanchette <William Blanchette>
2016-11-02 10:40:00 -0600

    Finished first pass at other building exterior prefabs

--- Assets/Scripts/DoorPressurePlate.cs
+++ Assets/Scripts/DoorPressurePlate.cs
@@ -6,23 +6,29 @@ public class DoorPressurePlate : MonoBehaviour {
     private AudioClip audioClip;
     private GameObject doorLeft;
     private GameObject doorRight;
+    private Vector3 doorLeftOriginalPosition;
+    private Vector3 doorRightOriginalPosition;
+    private float doorOpenOffset = 5f;
 
     void Start() {
         audioSource = GetComponent<AudioSource>();
         audioClip = Resources.Load<AudioClip>("Sounds/DoorOpen");
         doorLeft = transform.parent.Find("Model/DoorLeft").gameObject;
         doorRight = transform.parent.Find("Model/DoorRight").gameObject;
+
+        doorLeftOriginalPosition = doorLeft.transform.localPosition;
+        doorRightOriginalPosition = doorRight.transform.localPosition;
     }
 
     void OnTriggerEnter(Collider collider) {
-        iTween.MoveAdd(doorLeft, iTween.Hash("z", 4f, "time", 0.3f, "easeType", iTween.EaseType.linear));
-        iTween.MoveAdd(doorRight, iTween.Hash("z", -4f, "time", 0.3f, "easeType", iTween.EaseType.linear));
+        iTween.MoveTo(doorLeft, iTween.Hash("x", doorLeftOriginalPosition.x - doorOpenOffset, "time", 0.3f, "easeType", iTween.EaseType.linear));
+        iTween.MoveTo(doorRight, iTween.Hash("x", doorLeftOriginalPosition.x + doorOpenOffset, "time", 0.3f, "easeType", iTween.EaseType.linear));
         audioSource.PlayOneShot(audioClip);
     }
 
     void OnTriggerExit(Collider collider) {
-        iTween.MoveAdd(doorLeft, iTween.Hash("z", -4f, "time", 0.3f, "easeType", iTween.EaseType.linear));
-        iTween.MoveAdd(doorRight, iTween.Hash("z", 4f, "time", 0.3f, "easeType", iTween.EaseType.linear));
+        iTween.MoveTo(doorLeft, iTween.Hash("x", doorLeftOriginalPosition.x, "time", 0.3f, "easeType", iTween.EaseType.linear));
+        iTween.MoveTo(doorRight, iTween.Hash("x", doorRightOriginalPosition.x, "time", 0.3f, "easeType", iTween.EaseType.linear));
         audioSource.PlayOneShot(audioClip);
     }
 }
